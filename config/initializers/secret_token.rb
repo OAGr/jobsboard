16af8cf4e049f0834c840c4867427b86ee5f3dbc
peterhurford <peter@peterhurford.com>
2013-11-04 14:35:27 -0500

    EA Jobs Board

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobsboard::Application.config.secret_key_base = '9b72bcb5a20b1077aad6aa77756a640a57a3c5228eb9c980f797e84e3097799e3269ac70d969e69a3f652f41fd2bf280664deba3fa59115faf860b4429599df7'
