210582716b0ab7b4561fa05f87c42486ff045f4a
peterhurford <peter@peterhurford.com>
2013-11-11 13:16:49 -0500

    Switch routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Jobsboard::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'jobs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
